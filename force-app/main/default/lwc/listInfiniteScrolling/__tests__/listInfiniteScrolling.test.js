92cccfb74c1deb7e08e379535ffc10ae95a25e7a
Heather Dykstra <missheatherlaree@gmail.com>
2020-07-16 12:51:29 -0600

    finalized tests

--- force-app/main/default/lwc/listInfiniteScrolling/__tests__/listInfiniteScrolling.test.js
+++ force-app/main/default/lwc/listInfiniteScrolling/__tests__/listInfiniteScrolling.test.js
@@ -34,18 +34,6 @@ describe('c-list-infinite-scrolling', () => {
             expect(dataTableEl.data).toEqual(mockAccountData.records);
         });
     });
-
-    /**it('triggers loadMoreData when the list is scrolled down', () => {
-    // Create element
-    const element = createElement('c-list-infinite-scrolling', {
-        is: ListInfiniteScrolling
-      });
-      document.body.appendChild(element);
-
-    // create a mock handler for loadMoreData
-    expect(true);
-  });**/
-
     it('displays an error when the error variable is set', () => {
         const MESSAGE = 'Error retrieving data';
 
@@ -64,5 +52,38 @@ describe('c-list-infinite-scrolling', () => {
             );
             expect(errorPanelEl.errors.body).toBe(MESSAGE);
         });
+    });
+    it('requests more data when scrolling reaches the bottom', () => {
+        const element = createElement('c-list-infinite-scrolling', {
+            is: ListInfiniteScrolling
+        });
+        document.body.appendChild(element);
+
+        getAccountsPaginatedAdapter.emit(mockAccountData);
+
+        return Promise.resolve()
+            .then(() => {
+                const dataTableEl = element.shadowRoot.querySelector(
+                    'lightning-datatable'
+                );
+
+                expect(dataTableEl).not.toBeNull();
+                expect(dataTableEl.data).toEqual(mockAccountData.records);
+                expect(
+                    getAccountsPaginatedAdapter.getLastConfig().pageToken
+                ).toBe(0);
+            })
+            .then(() => {
+                // Toggle checkbox to show details
+                const dataTableEl = element.shadowRoot.querySelector(
+                    'lightning-datatable'
+                );
+                dataTableEl.dispatchEvent(new CustomEvent('loadmore'));
+            })
+            .then(() => {
+                expect(
+                    getAccountsPaginatedAdapter.getLastConfig().pageToken
+                ).toBe(5);
+            });
     });
 });
