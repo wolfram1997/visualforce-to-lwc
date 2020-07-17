aecdca1963aed3afff0e3f51c49078cf0c4fb230
Heather Dykstra <missheatherlaree@gmail.com>
2020-07-17 08:39:52 -0600

    initial test

---
+++ force-app/main/default/lwc/pageMessagesDataRetrievalWrapper/__tests__/pageMessagesDataRetrievalWrapper.test.js
@@ -0,0 +1,28 @@
+import { createElement } from 'lwc';
+import PageMessagesDataRetrievalWrapper from 'c/pageMessagesDataRetrievalWrapper';
+
+describe('c-page-messages-data-retrieval-wrapper', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+    it('shows inner component using c-example-wrapper', () => {
+        const element = createElement(
+            'c-page-messages-data-retrieval-wrapper',
+            {
+                is: PageMessagesDataRetrievalWrapper
+            }
+        );
+        document.body.appendChild(element);
+
+        const exampleEl = element.shadowRoot.querySelector('c-example-wrapper');
+        expect(exampleEl).not.toBeNull();
+
+        const innerEl = exampleEl.querySelector(
+            'c-page-messages-data-retrieval'
+        );
+        expect(innerEl).not.toBeNull();
+    });
+});
